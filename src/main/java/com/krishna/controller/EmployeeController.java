cb5f826664d64474f65c1584dc5671798ed2ab36
kandivenkata <venkatakrishnareddy.kandise@gmail.com>
2025-05-29 06:34:38 +0530

    remove @Controller and added @@RestController

--- src/main/java/com/krishna/controller/EmployeeController.java
+++ src/main/java/com/krishna/controller/EmployeeController.java
@@ -3,21 +3,19 @@ package com.krishna.controller;
 import com.krishna.dto.EmployeeRequest;
 import com.krishna.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("employee")
-@Controller
+@RestController
 public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
 
     @PostMapping
-    @ResponseBody
     public String add(@RequestBody EmployeeRequest employeeRequest){
         employeeService.addEmployee(employeeRequest);
         return "successfully added";
